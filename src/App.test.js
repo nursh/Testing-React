04850e75014d7c83b2596ee908369314353a6f08
nursh <nsheikhmohammed@yahoo.com>
2018-10-03 15:07:52 -0400

    Implemented test for increment-button and counter-display elements in the App Component

--- src/App.test.js
+++ src/App.test.js
@@ -9,16 +9,21 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test('renders without crashing', () => {
   const wrapper = shallow(<App />);
   // add an html element to the component with data-test attribute to test it is rendering
+  // check its length then implement it
   const appComponent = wrapper.find(`[data-test='component-app']`);
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-
+  const wrapper = shallow(<App />);
+  const incrementButton = wrapper.find(`[data-test='increment-button']`);
+  expect(incrementButton.length).toBe(1);
 });
 
 test('renders counter display', () => {
-
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find(`[data-test='counter-display']`);
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at 0', () => {
