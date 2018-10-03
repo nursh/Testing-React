ffb5afaa19a75a72f70a31c2bb4968660ef75bea
nursh <nsheikhmohammed@yahoo.com>
2018-10-03 15:18:34 -0400

    Added common setup for shallow wrapper of app component

--- src/App.test.js
+++ src/App.test.js
@@ -6,25 +6,34 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+// Setup function to setup create a shallow wrapper for the App Component
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+}
+
 test('renders without crashing', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   // add an html element to the component with data-test attribute to test it is rendering
   // check its length then implement it
   const appComponent = wrapper.find(`[data-test='component-app']`);
   expect(appComponent.length).toBe(1);
 });
 
+
+// Check to see if an element exists on the page that is crucial to the application
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const incrementButton = wrapper.find(`[data-test='increment-button']`);
   expect(incrementButton.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const counterDisplay = wrapper.find(`[data-test='counter-display']`);
   expect(counterDisplay.length).toBe(1);
 });
+
+
 
 test('counter starts at 0', () => {
 
