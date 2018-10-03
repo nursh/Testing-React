59948dc53c26136b227d73cf52ac3dfc1fcf9048
nursh <nsheikhmohammed@yahoo.com>
2018-10-03 14:56:15 -0400

    Wrote base test strucures for counter app

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
@@ -7,5 +7,22 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
 
 });
